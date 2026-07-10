ba6dc1270e041d86a8a27a9aa365aeb2c7304c88
Hazar <hazarcar0353@gmail.com>
2026-07-10 04:34:34 +0300

    add showcase images and update dynamicpack version

--- tools/create_display.py
+++ tools/create_display.py
@@ -19,27 +19,37 @@ def update_readme_stats(total_mods, total_effects):
         return
 
     # Tamamen senin tasarladığın dinamik badge yapısı
-    readme_template = f"""![Effect Icon+](https://github.com/Arcdashckr/Effect-Icon-Plus/blob/main/images/title/effect_icon_plus_title_horizontal.png?raw=true) </br>
-<center>
-<img src="https://github.com/Arcdashckr/Effect-Icon-Plus/blob/main/images/display/effect_display_vanilla.png?raw=true"> </br>
+    readme_template = f"""<p align="center">
+    <img src="https://github.com/Arcdashckr/Effect-Icon-Plus/blob/main/images/title/effect_icon_plus_title_horizontal.png?raw=true"> </br>
+    <img src="https://github.com/Arcdashckr/Effect-Icon-Plus/blob/main/images/display/effect_display_vanilla.png?raw=true"> </br>
+</p>
 
-**Adds colored frames to the icons of the effects** </br>
-**Experience a more vibrant and colorful look** </br> </br>
+<p align="center">
+    <strong>Adds colored frames to the icons of the effects</strong> </br>
+    <strong>Experience a more vibrant and colorful look</strong> </br> </br>
+</p>
+
 ---
 
 <p align="center">
-    <img src="https://img.shields.io/badge/Supported_Mods-_?style=for-the-badge&logo=modrinth&logoColor=%2300AF5C&logoSize=64&label={total_mods}&labelColor=black&color=00753F&link=https%3A%2F%2Fgithub.com%2FArcdashckr%2FEffect-Icon-Plus%2Fblob%2Fmain%2Fcompatibility.md" alt="Supported Mods Count: {total_mods}">
+    <img src="https://img.shields.io/badge/Supported_Mods-_?style=for-the-badge&logo=modrinth&logoColor=%2300AF5C&logoSize=64&label=39&labelColor=black&color=00753F&link=https%3A%2F%2Fgithub.com%2FArcdashckr%2FEffect-Icon-Plus%2Fblob%2Fmain%2Fcompatibility.md" alt="Supported Mods Count: 39">
     &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-    <img src="https://img.shields.io/badge/Total_Effect_Icons-_?style=for-the-badge&logo=buffer&logoColor=%23FFFFFF&logoSize=64&label={total_effects}&labelColor=black&color=blue&link=https%3A%2F%2Fgithub.com%2FArcdashckr%2FEffect-Icon-Plus%2Ftree%2Fmain%2Fassets" alt="Total Effects Count: {total_effects}">
+    <img src="https://img.shields.io/badge/Total_Effect_Icons-_?style=for-the-badge&logo=buffer&logoColor=%23FFFFFF&logoSize=64&label=244&labelColor=black&color=blue&link=https%3A%2F%2Fgithub.com%2FArcdashckr%2FEffect-Icon-Plus%2Ftree%2Fmain%2Fassets" alt="Total Effects Count: 244">
 </p>
 
-<img src="https://github.com/Arcdashckr/Effect-Icon-Plus/blob/main/images/display/effect_display_mod.png?raw=true"> </br>
-**Check mod compatibility list from <a href="https://github.com/Arcdashckr/Effect-Icon-Plus/blob/main/compatibility.md" target="_blank">here</a>** </br>
-**Request mod compatibility from <a href="https://github.com/Arcdashckr/Effect-Icon-Plus/issues" target="_blank">here</a>** </br> </br>
+<p align="center">
+    <strong>Check mod compatibility list from <a href="https://github.com/Arcdashckr/Effect-Icon-Plus/blob/main/compatibility.md" target="_blank">here</a></strong> </br>
+    <strong>Request mod compatibility from <a href="https://github.com/Arcdashckr/Effect-Icon-Plus/issues" target="_blank">here</a></strong> </br> </br>
+    <img src="https://github.com/Arcdashckr/Effect-Icon-Plus/blob/main/images/display/effect_display_mod.png?raw=true"> </br>
+</p>
+
 ---
 
+<div align="center">
+
 ### <a href="https://github.com/Arcdashckr/Effect-Icon-Plus/blob/main/permission/" target="_blank">*</a>Thanks to <a href="https://www.planetminecraft.com/texture-pack/upgrade-effect-icon/" target="_blank">Upgrade Effect Icon</a> by <a href="https://www.planetminecraft.com/member/the_dark_side_of_rero/" target="_blank">CherryRero</a>
-</center>"""
+
+</div>"""
 
     try:
         with open(README_PATH, "w", encoding="utf-8") as f:
